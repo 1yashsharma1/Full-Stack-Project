4e42f4707e9f7cef146c8f272774c0a7c2fe97b6
1yashsharma1 <syash832@gmail.com>
2023-08-10 22:37:28 +0530

    First Commit adding initial project files

---
+++ src/main/java/com/campusguru/CampusGuruApplication.java
@@ -0,0 +1,13 @@
+package com.campusguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusGuruApplication.class, args);
+	}
+
+}
